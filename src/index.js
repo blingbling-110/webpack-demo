9fd3d993ca368e93c14fcd639f04e8f7efdf90cc
qinzijun <tanzijun@gangtise.com.cn>
2021-06-11 18:53:35 +0800

    chore: 回滚到getting-started

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import _ from 'lodash';
-import printMe from './print.js';
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+ function component() {
+   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // lodash 在当前 script 中使用 import 引入
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+   return element;
+ }
 
-  element.appendChild(btn);
-
-  return element;
-}
-
-document.body.appendChild(component());
+ document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: '开发环境'
-    }),
-  ],
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/'
-  }
+  },
 };
