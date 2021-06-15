517484e68d9a530b5b3d629e1b7dfded1f6ba141
qinzijun <tanzijun@gangtise.com.cn>
2021-06-15 14:09:06 +0800

    feat: 缓存

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js'
-  },
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '缓存'
+    })
+  ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
+  },
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
   }
 };
